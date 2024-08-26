28b81342d27a0f868dcc027f35eaadcc75af5e20
mrdarip <darioelas2005@gmail.com>
2024-08-26 21:46:03 +0200

    reimplemented fetch to test if it allows to fetch from github pages as they share domain

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,9 @@
 console.log('fetcher.js');
 
-    var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "https://github.com/mrdarip?tab=repositories", true);
-xhttp.send();
+fetch("https://github.com/mrdarip?tab=repositories").then(response => {
+    console.log('fetcher.js response');
+    return response.text();
+    }).then(text => {
+        console.log('fetcher.js text');
+        console.log(text);
+    });
